883f08aa60643702565c8c204675ade4c3363d5c
telekrex <telekrex@gmail.com>
2023-03-18 11:09:54 -0400

    designed

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# basilisk
-Encryption cipher unlocked with a key word
+# Basilisk
+
+A cipher I designed that combines polygraph, transposition, and a sort of private-key. They key word makes the transposing sequence unique, so it's near impossible to crack without the key word. It was mainly for fun, but I'm optimistic that it could serve a purpose for someone. A basic knowledge of python scripts should be all you need to make it useful. It should be compatible with pen on paper as well, since it uses only math and sequences, and no python-specific functions. You'd just have to spend some time copying the engineering. I'd prefer to leave the explicit just to code, but if someone thinks it should be explained in this readme, contact me. I'm also open to any suggestions or contribution ideas.
+
+### Install
+
+*Requires: Python 3*  
+1. Install Python 3 if you don't have it  
+2. Download or clone this repository  
+
+
+### Encoding
+`python basilisk.py <encode> <message> <keyword>`  
+
+### Decoding
+`python basilisk.py <decode> <message> <keyword>`  
+
+- Something worth mentioning is that these functions lazilly accept anything as input. It is intended that the keyword and/or message does not include spaces, symbols, and numbers. I could add sanitizing to sturdy up those functions if needed.
---
+++ basilisk.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+
+#
+#                    BASILISK
+#
+#                      _/
+#                   __~a~_/
+#                   ~~;  ~_/
+#     _                ~  ~_/                _
+#    '_\;__._._._._._._]   ~_._._._._._.__;/_'
+#    '(/'/'/'/'|'|'|'| (    )|'|'|'|'\'\'\'\)'
+#    (/ / / /, | | | |(/    \) | | | ,\ \ \ \)
+#   (/ / / / / | | | ^(/    \) ^ | | \ \ \ \ \)
+#  (/ / / / /  ^ ^ ^   (/  \)    ^ ^  \ \ \ \ \)
+# (/ / / / ^           /(||)\          ^ \ \ \ \)
+# ^ / / ^             M /||\ M            ^ \ \ ^
+#  ^ ^                  /||\                 ^ ^
+#                      //||\\
+#                      //||\\
+#                      //||\\
+#                      '/||\'
+#
+
+
+import sys
+
+
+alphabet = [
+				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+				'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+				'u', 'v', 'w', 'x', 'y', 'z'
+			]
+alphebet = [
+				'x', 'e', 'o', 'y', 'z', 'k', 'n', 't', 'b', 'c',
+				'v', 'i', 'r', 's', 'w', 'q', 'a', 'd', 'l', 'u',
+				'j', 'f', 'g', 'm', 'h', 'p'
+			]
+
+
+def key(keyword):
+
+	characters = [*keyword]
+	numbers = []
+
+	for x in range(7):
+		for character in characters:
+			x = characters.index(character) # this letter in the keyword
+			a = alphabet.index(character) # this letter's index in the alphabet
+			try:
+				y = characters[x + 1] # the next letter in the keyword
+			except:
+				y = characters[0] # the next letter in the keyword
+			b = alphabet.index(y) # the next letter's index in the alphabet
+			numbers.append(str(abs(a - b)))
+
+	key = str(''.join(numbers)) # like teeth on a key
+	return key
+
+
+def encode(message, keyword):
+
+	sequence = key(keyword)
+	characters =[*message]
+	transpositions = [*sequence]
+	results = []
+
+	for index, character in enumerate(characters):
+		position = alphabet.index(character)
+		transposition = int(transpositions[index])
+		x = alphebet[position + transposition]
+		results.append(x)
+
+	return str('').join(results)
+
+
+def decode(codetxt, keyword):
+
+	sequence = key(keyword)
+	characters =[*codetxt]
+	transpositions = [*sequence]
+	results = []
+
+	for index, character in enumerate(characters):
+		position = alphebet.index(character)
+		transposition = int(transpositions[index])
+		x = alphabet[position - transposition]
+		results.append(x)
+
+	return str('').join(results)
+
+
+if __name__ == '__main__':
+
+	if sys.argv[1] == 'encode':
+		print(encode(sys.argv[2], sys.argv[3]))
+
+	if sys.argv[1] == 'decode':
+		print(decode(sys.argv[2], sys.argv[3]))
